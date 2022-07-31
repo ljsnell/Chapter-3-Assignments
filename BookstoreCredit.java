9009642345b63ffb3457edc28234529e0dc47745
ljsnell <ljsnell7@gmail.com>
2022-07-30 22:28:01 -0500

    Initial solution for bookstore credit
    Need to get user input next.

--- BookstoreCredit.java
+++ BookstoreCredit.java
@@ -1,5 +1,13 @@
 public class BookstoreCredit {
     public static void main(String[] args) {
+        String studentName = "Bill";
+        double studentGPA = 3.2;
 
+        calculateBookStoreCredit(studentName, studentGPA);
+    }
+
+    public static void calculateBookStoreCredit(String studentsName, double studentsGPA) {
+        System.out.println(studentsName + " with their GPA of: " +studentsGPA +
+                        " has received a credit of: $" + studentsGPA * 10);
     }
 }
