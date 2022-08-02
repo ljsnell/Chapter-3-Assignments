5ce4ae4aed7d3232f8f5cc55412c785d1a874322
ljsnell <ljsnell7@gmail.com>
2022-08-01 21:00:39 -0500

    Update Lease.java
    13a done I believe.

--- Lease.java
+++ Lease.java
@@ -1,5 +1,54 @@
 public class Lease {
-    public static void main(String[] args) {
-        
+    String tenantsName;
+    String apartmentNumber;
+    int monthlyRentAmount;
+    int monthsOfLease;
+
+    public Lease() {
+        tenantsName = "XXX";
+        apartmentNumber = "0";
+        monthlyRentAmount = 1000;
+        monthsOfLease = 12;
+    }
+
+    private void addPetFee() {
+        monthlyRentAmount += 10;
+        explainPetPolicy();
+    }
+
+    private static void explainPetPolicy() {
+        System.out.println("We charge an extra $10 a month for pets.");
+    }
+    // Getters And Setters
+    public String getTenantsName() {
+        return tenantsName;
+    }
+
+    public void setTenantsName(String tenantsName) {
+        this.tenantsName = tenantsName;
+    }
+
+    public String getApartmentNumber() {
+        return apartmentNumber;
+    }
+
+    public void setApartmentNumber(String apartmentNumber) {
+        this.apartmentNumber = apartmentNumber;
+    }
+
+    public int getMonthlyRentAmount() {
+        return monthlyRentAmount;
+    }
+
+    public void setMonthlyRentAmount(int monthlyRentAmount) {
+        this.monthlyRentAmount = monthlyRentAmount;
+    }
+
+    public int getMonthsOfLease() {
+        return monthsOfLease;
+    }
+
+    public void setMonthsOfLease(int monthsOfLease) {
+        this.monthsOfLease = monthsOfLease;
     }
 }
