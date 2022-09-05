86570790b05593aac7a3182046fe7e86515aea2c
ljsnell <ljsnell7@gmail.com>
2022-09-05 13:25:48 -0500

    Added sandwich code.

---
+++ sandwich/Sandwich.java
@@ -0,0 +1,26 @@
+package sandwich;
+
+public class Sandwich {
+	String mainIngredient;	
+	String bread;
+	double price;
+
+	public String getMainIngredient() {
+		return mainIngredient;
+	}
+	public void setMainIngredient(String mainIngredient) {
+		this.mainIngredient = mainIngredient;
+	}
+	public String getBread() {
+		return bread;
+	}
+	public void setBread(String bread) {
+		this.bread = bread;
+	}
+	public double getPrice() {
+		return price;
+	}
+	public void setPrice(double price) {
+		this.price = price;
+	}
+}
---
+++ sandwich/TestSandwich.java
@@ -0,0 +1,11 @@
+package sandwich;
+
+public class TestSandwich {
+
+	public static void main(String[] args) {
+		Sandwich exampleSammy = new Sandwich();
+		exampleSammy.setBread("wheat");
+		System.out.println(exampleSammy.getBread());
+	}
+
+}
