bfb6e07851b99b20c6d7e02d9dc143357e1cc523
ljsnell <ljsnell7@gmail.com>
2022-09-05 13:14:16 -0500

    Added JobPricing and Removed Lease

---
+++ JobPricing.java
@@ -0,0 +1,13 @@
+import javax.swing.JOptionPane;
+
+public class JobPricing {
+
+	public static void main(String[] args) {		
+		
+	}
+	
+	public static int calculateTotalCost(int costOfMaterials, int numberOfHoursWorked, int numberOfHoursDriven) {
+		
+	}
+
+}
